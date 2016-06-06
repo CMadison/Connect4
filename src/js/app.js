83bca3fca40654351e8a87e4b83e76f3eecdb0ef
Chris Madison <CKMadison4@gmail.com>
2016-06-06 13:19:41 -0700

    Added gameboard and divs for each cell. Next step is to edit cells to match visual structure of table element.

---
+++ src/js/app.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+// Global variable that keeps track of user turn. Set this as true (player 1) or false (player 2). This variable will be set to the opposite on button click of current player's turn.
+var playerOneTurn = true;
+
+// Gameboard
+
+
+
+
+
+
+// Create 3 seperate functions to test for winning conditions (horizontal, vertical, and diagonal). All should be in one overarching function to test winning condition.
+        // For winning conditions, you need to test each cell.
+
+});
