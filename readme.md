eeabeb3e6b07d40c4e18d21ad6baf0b03aef1de0
Chris Madison <CKMadison4@gmail.com>
2016-06-09 07:35:40 -0700

    Mostly complete game see readme for remaining to do items.

--- readme.md
+++ readme.md
@@ -10,3 +10,18 @@ This will be a connect 4 board game clone for a class project.
 * `npm start` - start the BrowserSync server
 * `npm run lint:css` - lint CSS
 * `npm run lint:js` - lint JS
+
+##Rules
+* The Rules Are Simple
+
+The Connect 4 Board Game Rules are easy to understand. In fact, it is in the name. To win Connect Four, all you have to do is connect four of your colored checker pieces in a row, much the same as tic tac toe. This can be done horizontally, vertically or diagonally. Each player will drop in one checker piece at a time. This will give you a chance to either build your row, or stop your opponent from getting four in a row.
+
+The game is over either when you or your friend reaches four in a row, or when all forty two slots are filled, ending in a stalemate. If you and your friend decide to play again, the loser typically goes first. Cleaning up the game is very easy. The bottom of the vertical game board has a switch that you slide to make the pieces drop out of the board. This is a great opportunity to put the box underneath the game board to collect all of checker pieces together.
+
+The rules of the game are easy to learn, but difficult to master. That is the beauty of Connect Four. Now that you know the Connect 4 board game rules, now is the time to challenge everyone you know. No matter their age or skill level, they can play this game with you. Now that you understand the rules, share Connect Four with everyone around you. You’ll be glad you did.
+
+##Outstanding Items
+* Add win conditions for diagonal wins
+* Add more attractive alert pop up for starting a new game
+* Refactor code to make it more modular i.e. displaying which player actually won vs. a generic you won message
+* Properly indent and comment
--- src/js/app.js
+++ src/js/app.js
@@ -4,7 +4,6 @@ $(document).ready(function() {
 
 
 // Get all gameboard elements row 0. Create 2d array to hold all references. Use for loop to iterate through and then concatenate as string and store in array.
-// 2D array does not go away create object for each game space and store value of game space i.e. 0,1,2 and element of game space i.e. 00,06,60, etc.
 
 // This generates the gameboard dynamically, but I need to figure out how to make the variable global so it can be accessed by other functions
 
@@ -36,7 +35,6 @@ var gbArray = gbfun(7,7,0);
 
 var playerOneTurn = true;
 
-
 // create a for loop that checks each array position in a column and runs logic if not null check next cell in column else set gbarray = to space in which
 
 $("#row0 .cell").on('click', playpiece);
@@ -58,11 +56,11 @@ for (var row = 6; row >= 0; row--) {
     console.log(gbArray);
     console.log(playerOneTurn);
 
-// vertical win or right win
-        if (/*south win*/gbArray[row+1]&&gbArray[col][row] == gbArray[col][row+1] && gbArray[row+2]&&gbArray[col][row] == gbArray[col][row+2] && gbArray[row+3]&&gbArray[col][row] == gbArray[col][row+3] || /*west win*/(gbArray[col-1]&&gbArray[col][row] == gbArray[col-1][row]) && gbArray[col-2]&&gbArray[col][row] == gbArray[col-2][row] && gbArray[col-3]&&gbArray[col][row] == gbArray[col-3][row] || /*north win*/gbArray[row-1]&&gbArray[col][row] == gbArray[col][row-1] && gbArray[row-2]&&gbArray[col][row] == gbArray[col][row-2] && gbArray[row-3]&&gbArray[col][row] == gbArray[col][row-3] || /*east win*/gbArray[col+1]&&gbArray[col][row] == gbArray[col+1][row] && gbArray[col+2]&&gbArray[col][row] == gbArray[col+2][row] && gbArray[col+3]&&gbArray[col][row] == gbArray[col+3][row]) {
-          alert("Player 1 is the winner");
-        }
-    return (gbArray[col][row]);
+
+     verticalWin();
+     horizontalWin()
+     return;
+
     }
   else {
     gbArray[col][row] = 2;
@@ -70,8 +68,11 @@ for (var row = 6; row >= 0; row--) {
     playerOneTurn = !playerOneTurn;
     console.log(gbArray);
     console.log(playerOneTurn);
+    verticalWin();
+    horizontalWin()
     return (gbArray[col][row]);
 }
+
 
 // Create 3 seperate functions to test for winning conditions (horizontal, vertical, and diagonal). All should be in one overarching function to test winning condition.
         // For winning conditions, you need to test each cell.
@@ -88,10 +89,75 @@ for (var row = 6; row >= 0; row--) {
    $('#newGameBtn').click(function() {
     location.reload();
     });
+
+// Borrowed and adapted from https://github.com/bryanbraun/connect-four
+
+function verticalWin() {
+    var currentValue = null,
+        previousValue = 0,
+        tally = 0;
+
+    // Scan each row in series, tallying the length of each series. If a series
+    // ever reaches four, return true for a win.
+    for (var col = 0; col <= 5; col++) {
+        for (var row = 0; row <= 6; row++) {
+            currentValue = gbArray[col][row];
+            if (currentValue === previousValue && currentValue !== 0) {
+                tally += 1;
+            } else {
+                // Reset the tally if you find a gap.
+                tally = 0;
+            }
+            if (tally === 4 - 1) {
+                swal("You win! Be a good sport about it, and play another match.")
+                $("#row0 .cell").unbind("click");
+                return;
+            }
+            previousValue = currentValue;
+        }
+
+        // After each row, reset the tally and previous value.
+        tally = 0;
+        previousValue = 0;
+    }
+
+    // No horizontal win was found.
+    return;
+}
+
+function horizontalWin() {
+    var currentValue = null,
+        previousValue = 0,
+        tally = 0;
+
+    // Scan each column in series, tallying the length of each series. If a
+    // series ever reaches four, return true for a win.
+    for (var row = 0; row <= 6; row++) {
+        for (var col = 0; col <= 5; col++) {
+            currentValue = gbArray[col][row];
+            if (currentValue === previousValue && currentValue !== 0) {
+                tally += 1;
+            } else {
+                // Reset the tally if you find a gap.
+                tally = 0;
+            }
+            if (tally === 4 - 1) {
+                swal("You win! Be a good sport about it, and play another round.")
+                $("#row0 .cell").unbind("click");
+                return true;
+            }
+            previousValue = currentValue;
+        }
+
+        // After each column, reset the tally and previous value.
+        tally = 0;
+        previousValue = 0;
+    }
+
+    // No vertical win was found.
+    return false;
+}
 });
-
-
-
 // Extra Code that works dont touch
 
 
@@ -104,3 +170,20 @@ for (var row = 6; row >= 0; row--) {
 //               [0,0,0,0,0,0,0],
 //               [0,0,0,0,0,0,0],
 //               [0,0,0,0,0,0,0]];
+
+
+// // vertical win or right win
+//  if (/*south win*/gbArray[row+1]&&gbArray[col][row] == gbArray[col][row+1] && gbArray[row+2]&&gbArray[col][row] == gbArray[col][row+2] && gbArray[row+3]&&gbArray[col][row] == gbArray[col][row+3]
+
+//  /*west win*/(gbArray[col-1]&&gbArray[col][row] == gbArray[col-1][row]) && gbArray[col-2]&&gbArray[col][row] == gbArray[col-2][row] && gbArray[col-3]&&gbArray[col][row] == gbArray[col-3][row]
+
+// || /*north win*/gbArray[row-1]&&gbArray[col][row] == gbArray[col][row-1] && gbArray[row-2]&&gbArray[col][row] == gbArray[col][row-2] && gbArray[row-3]&&gbArray[col][row] == gbArray[col][row-3]
+
+// || /*east win*/gbArray[col+1]&&gbArray[col][row] == gbArray[col+1][row] && gbArray[col+2]&&gbArray[col][row] == gbArray[col+2][row] && gbArray[col+3]&&gbArray[col][row] == gbArray[col+3][row]
+
+// || /*northeast win*/gbArray[col+1]&&gbArray[col][row]&&[row-1]&&gbArray[col][row] == gbArray[col+1][row-1] && gbArray[col+2][row-2]&&gbArray[col][row] == gbArray[col+2][row-2] && gbArray[col+3][row-3]&&gbArray[col][row] == gbArray[col+3][row-3]
+
+// || /*northwest win*/gbArray[col-1]&&gbArray[col][row][row-1]&&gbArray[col][row] == gbArray[col-1][row-1] && gbArray[col-2][row-2]&&gbArray[col][row] == gbArray[col-2][row-2] && gbArray[col-3][row-3]&&gbArray[col][row] == gbArray[col-3][row-3]) {
+
+
+//     }
